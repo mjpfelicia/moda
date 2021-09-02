55a0ed9e8924620e8a1f4e17dabf595f05633e7d
sandra <mjpfelicia@gmail.com>
2021-09-02 16:43:07 -0300

    correçao total

--- script.js
+++ script.js
@@ -1,29 +1,18 @@
-// const inputEmail = document.querySelector("input#email");
-
-
-// if (input.validity.typeMismatch) {
-//     input.setCustomValidity("Dude '" + input.value + "' não é um e-mail válido. Digite algo legal !!");
-// }
-// outro {
-//     input.setCustomValidity("");
-// }
+const emailValido = (email = "") => {
+    const regex = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+    return regex.test(email);
+}
 
 function validaForm() {
     const input = document.querySelector('input');
     const erroMsg = document.querySelector('#emalErrorMsg');
+    const form = document.querySelector('form');
+    const inputError = document.querySelector('#inputError');
 
-
-    input.oninvalid = (e) => {
-
-        e.target.setCustomValidity("");
-
-        if (!e.target.validity.valid) {
-            e.target.setCustomValidity("");
-
-            erroMsg.style.display = 'block';
-
-        }
-        console.log({ e });
-
+    if (!emailValido(input.value)) {
+        inputError.style.display = 'block';
+        erroMsg.style.display = 'block';
+        form.style.border = '1px solid red';
     }
+
 }
